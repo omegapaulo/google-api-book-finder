f6b488efaa257ba0d034608a6c92cc4111381688
Paulo Omega <paulodafiotiomega@gmail.com>
2020-09-15 16:39:46 +0300

    Fixes on the css and javascript

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,17 @@
 document.querySelector('.header--form').addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  // const inputValue = document.querySelector('.search').value;
+  const inputValue = document.querySelector('.search').value;
 
-  // const dataObj = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${inputValue}&maxResults=40&projection=full&printType=books`);
-  const dataObj = await fetch('harrypotter.json');
+  const dataObj = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${inputValue}&maxResults=40&projection=full&printType=books`);
+  // const dataObj = await fetch('harrypotter.json');
 
   const dataStatus = await dataObj.json();
 
   dataStatus.items.forEach((book) => {
     const bookInfo = book.volumeInfo;
 
-    console.log(bookInfo);
+    // console.log(bookInfo);
 
     if (bookInfo.description !== undefined) {
       document.querySelector('.book--cards').innerHTML += `
@@ -22,7 +22,7 @@ document.querySelector('.header--form').addEventListener('submit', async (e) => 
       <ul>
       <li class="book--title">Author: <span>${bookInfo.authors}</span></li>
       <li class="book--author">Published: <span>${bookInfo.publishedDate}</span></li>
-      <button id="btn">See this Book</button>
+      <button  id="btn"><a href="${bookInfo.previewLink}" target="_blank">See this Book</a></button>
       </ul>
       </div>
         <p>${bookInfo.description}</p>
@@ -33,3 +33,9 @@ document.querySelector('.header--form').addEventListener('submit', async (e) => 
     document.querySelector('.search').value = '';
   });
 });
+
+function resetUi() {
+  document.querySelector('.book--cards').innerHTML = '';
+}
+
+document.querySelector('.search').addEventListener('keyup', resetUi);
