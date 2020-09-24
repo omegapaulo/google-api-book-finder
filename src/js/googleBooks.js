f5eaea2e6d85ae985e38deb8fef74a6f1872ca42
Paulo Omega <paulodafiotiomega@gmail.com>
2020-09-24 23:29:34 +0300

    Edited files

--- src/js/googleBooks.js
+++ src/js/googleBooks.js
@@ -5,7 +5,7 @@ export default class Books {
 
   static checkFetch(response) {
     if (!response.ok) {
-      console.log(response);
+      // console.log(response);
       throw Error(`The status is ${response.status} and check the url ${response.url} to handle errors, this is the error message ${response.statusText}`);
     }
     return response;
@@ -14,13 +14,15 @@ export default class Books {
   async getBooks(book) {
     try {
       this.bookRequests = await fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=${book}&maxResults=40&projection=full&printType=books&key=${this.Api_Key}`
+        `https://www.googleapis.com/books/v1/volumes?q=${book}&maxResults=40&projection=full&printType=books&key=${this.Api_Key}`,
       );
 
       // Checking if request status is ok
       Books.checkFetch(this.bookRequests);
 
       this.booksInfo = await this.bookRequests.json();
+
+      console.log(this.booksInfo);
     } catch (error) {
       // Log error for the user through ui.js
       console.log(error);
